0619ec5246d0190fdadada394fee838d068074fc
adrinaVaz14 <vazadrina22@gmail.com>
2024-06-25 09:43:26 -0700

    Initial Commit

---
+++ KWK.day4/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  KWK.day4
+//
+//  Created by Scholar on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var name = ""
+    @State var textTitle = "What is your name?"
+    
+    var body: some View {
+        
+        VStack {
+            Text (textTitle)
+                .font(.largeTitle)
+            TextField("Please type in your name...", text: $name)
+                .multilineTextAlignment(.center)
+                .border(Color.gray, width: 1)
+            
+            Button("Submit Name") {
+                textTitle = "Welcome, \(name)"
+                
+                
+                
+                
+            } // closes button
+            .font(.title)
+            .buttonStyle(.borderedProminent)
+            .tint(.mint)
+            
+            
+            
+        } // closes vstack
+    } // closes body
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KWK.day4/KWK_day4App.swift
@@ -0,0 +1,17 @@
+//
+//  KWK_day4App.swift
+//  KWK.day4
+//
+//  Created by Scholar on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct KWK_day4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
